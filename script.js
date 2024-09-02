9bbb2d0129534ef63633cc839ed7342250d3e4be
Armin Esmaili <armines765@gmail.com>
2024-09-02 21:14:27 +0200

    About and Projects added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 var i = 0;
 var txt = 'Armin Esmaili'; /* The text */
 var speed = 200; /* The speed/duration of the effect in milliseconds */
@@ -28,3 +31,29 @@ function typeremover() {
 window.onload = function() {
     typeWriter();
 };
+
+
+
+
+
+
+
+ // script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    var navbar = document.querySelector('.navbar');
+
+    function handleScroll() {
+        if (window.scrollY > 200) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    }
+
+    // Initial check in case the page is already scrolled
+    handleScroll();
+
+    // Add event listener for scroll
+    window.addEventListener('scroll', handleScroll);
+});
