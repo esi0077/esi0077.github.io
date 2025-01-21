be14c403f8632037ee0809681419aa78e8360fbb
Armin Esmaili <armines765@gmail.com>
2025-01-21 20:50:34 +0100

    change everything ?
    changed the site fully

--- script.js
+++ script.js
@@ -1,79 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Typewriter effect initialization
-    var i = 0;
-    var txt = 'Armin Esmaili'; /* The text */
-    var speed = 200; /* The speed/duration of the effect in milliseconds */
-
-    function typeWriter() {
-        if (i < txt.length) {
-            document.getElementById("ArminType").innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        } else {
-            setTimeout(typeremover, 1000);
-        }
-    }
-
-    function typeremover() {
-        if (i > 0) {
-            document.getElementById("ArminType").innerHTML = txt.substring(0, i - 1);
-            i--;
-            setTimeout(typeremover, speed);
-        } else {
-            setTimeout(typeWriter, 1000);
-        }
-    }
-
-    // Start the typewriter effect
-    typeWriter();
-
-    // Navbar scroll effect
-    var navbar = document.querySelector('.navbar');
-
-    function handleScroll() {
-        if (window.scrollY > 200) {
-            navbar.classList.add('scrolled');
-        } else {
-            navbar.classList.remove('scrolled');
-        }
-    }
-
-    // Initial check and scroll event listener
-    handleScroll();
-    window.addEventListener('scroll', handleScroll);
-
-    // Image visibility on scroll
-    function handleImageVisibility() {
-        var image = document.getElementById('ArminPicture');
-        var imagePosition = image.getBoundingClientRect().top;
-        var screenPosition = window.innerHeight;
-
-        if (imagePosition < screenPosition) {
-            image.classList.add('visible');
-        }
-    }
-
-    // Check image visibility on scroll and on page load
-    window.addEventListener('scroll', handleImageVisibility);
-    handleImageVisibility();
-
-    // Initialize AOS (Animate on Scroll)
-    AOS.init();
-});
-document.getElementById('hamburger-menu').onclick = function() {
-    var mobileNav = document.getElementById('mobile-nav');
-    if (mobileNav.classList.contains('active')) {
-        mobileNav.classList.remove('active');
-    } else {
-        mobileNav.classList.add('active');
-    }
-};
-
-// Close the mobile nav when a link is clicked
-document.querySelectorAll('.mobile-nav a').forEach(function(link) {
-    link.onclick = function() {
-        var mobileNav = document.getElementById('mobile-nav');
-        mobileNav.classList.remove('active');
-    };
-});
-
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
