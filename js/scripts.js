ce90fb1874e4b5b22059e1a4213a8e551495dbc8
Armin Esmaili <armines765@gmail.com>
2025-02-02 19:42:15 +0100

    updated form

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,31 @@ window.addEventListener('load', function() {
         }, 500); 
     }, 700); 
 });
+
+
+// web3 forms
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    const formData = new FormData(this);
+
+    fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            document.getElementById("submitSuccessMessage").classList.remove("d-none");
+            document.getElementById("submitErrorMessage").classList.add("d-none");
+            this.reset(); // Reset form fields after success
+        } else {
+            document.getElementById("submitSuccessMessage").classList.add("d-none");
+            document.getElementById("submitErrorMessage").classList.remove("d-none");
+        }
+    })
+    .catch(() => {
+        document.getElementById("submitSuccessMessage").classList.add("d-none");
+        document.getElementById("submitErrorMessage").classList.remove("d-none");
+    });
+});
